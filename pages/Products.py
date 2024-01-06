d483958f8fa46d563e068c13be6e411d096f2050
nsmuthuram <154507835+nsmuthuram@users.noreply.github.com>
2024-01-06 13:34:56 +0530

    Update Products.py

--- pages/Products.py
+++ pages/Products.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_files_connection import FilesConnection
 import pandas as pd
 
 st.set_page_config(
@@ -6,20 +7,17 @@ st.set_page_config(
     page_icon="👋",
 )
 
-
 st.title('Product Report')
 
-df= pd.read_excel(
-      io='supermarkt_sales.xlsx',
-      #engine='openpyxl',
-      sheet_name='Sales',
-      skiprows=3,
-      usecols='G, C, H',
-      nrows=1000,
-    )
+# Create connection object and retrieve file contents.
+conn = st.connection('s3', type=FilesConnection)
 
-st.dataframe(   df
-            )
+# Specify input format is a csv and to cache the result for 600 seconds.
+df = conn.read("msawsbuckets3/supermarkt_sales.csv", input_format="csv", usecols=["Product line", "Branch", "Unit price"], ttl=600)
+
+st.data_editor(df, use_container_width=True, hide_index=True)
+#st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+#st.data_frame(df, use_container_width=True, hide_index=True)
 
 # HIDE STREAMLIT STYLE
 hide_st_style="""
